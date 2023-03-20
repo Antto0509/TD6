99c88445960f476ad0cb16a19f8f63cf88c13f80
Antto <coutreelantoine@gmail.com>
2023-03-20 16:51:33 +0100

    Ajout sous-programme Moyenne

--- Git/Program.cs
+++ Git/Program.cs
@@ -13,7 +13,13 @@ int Oppose(int a)
     return -a;
 }
 
+float Moyenne(int a, int b)
+{
+    return (a + b) / 2.0;
+}
+
 Console.WriteLine("Bienvenue");
 Console.WriteLine(Somme(2, 3));
 Console.WriteLine(Produit(6, 8));
 Console.WriteLine(Oppose(-7));
+Console.WriteLine(Moyenne(2,3));
