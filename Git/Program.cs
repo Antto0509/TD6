3c85096c9de0dbc30409c2ea0208aca8669b6270
Antto <coutreelantoine@gmail.com>
2023-03-20 16:26:39 +0100

    Ajout sous-programme Oppose

--- Git/Program.cs
+++ Git/Program.cs
@@ -8,5 +8,11 @@ int Produit(int x, int y)
     return x * y;
 }
 
+int Oppose(int a)
+{
+    return -a;
+}
+
 Console.WriteLine(Somme(2, 3));
 Console.WriteLine(Produit(6, 8));
+Console.WriteLine(Oppose(-7));
